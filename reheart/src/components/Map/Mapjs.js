0e54749c507736975dd32055a15a3097905d92b2
dnwntjsg <97648066+songyeon7@users.noreply.github.com>
2023-10-23 20:10:30 +0900

    지도 수정중

--- reheart/src/components/Map/Mapjs.js
+++ reheart/src/components/Map/Mapjs.js
@@ -1,131 +1,77 @@
-import React, { useState, useEffect } from 'react';
+import mapJson from '/files/data.json' assert {type: 'json'};
 
-const Mapjs = () => {
-    const [map, setMap] = useState(null);
-    const [markers, setMarkers] = useState([]); // Define the markers state variable
-    const [searchInput, setSearchInput] = useState('');
-
-    useEffect(() => {
-        // ComponentDidMount 로직
-        const mapOption = {
-            center: new window.kakao.maps.LatLng(37.542268, 126.967049),
-            level: 3,
-        };
-
-        const initializedMap = new window.kakao.maps.Map(document.getElementById('map'), mapOption);
-        setMap(initializedMap);
-
-        // ... (위 코드에서 변경 없음)
-
-        // 위치 이름 기반 검색 함수
-        function handleSearch() {
-            markers.forEach((marker) => marker.setMap(null)); // 기존 마커 제거
-            setMarkers([]); // 마커 배열 초기화
-
-            for (let i = 0; i < 5000; i++) {
-                const stringVal = window.mapJson.data[i].buildaddress;
-                const substring = searchInput;
-
-                if (stringVal.includes(substring)) {
-                    const markerPosition = new window.kakao.maps.LatLng(window.mapJson.data[i].wgs84lat, window.mapJson.data[i].wgs84lon);
-                    // 마커를 생성합니다
-                    const marker = new window.kakao.maps.Marker({
-                        map: map,
-                        position: markerPosition,
-                        title: window.mapJson.data[i].buildplace,
-                    });
-                    setMarkers((prevMarkers) => [...prevMarkers, marker]);
-                }
-            }
-        }
-    }, []);
-
-    // ... (위 코드에서 변경 없음)
-
-    const mapStyles = {
-        width: '100%',
-        height: '500px',
-    };
-
-    const searchInputStyles = {
-        width: '200px',
-        height: '30px',
-        marginRight: '10px',
-    };
-
-    const searchButtonStyles = {
-        width: '50px',
-        height: '30px',
-    };
-
-    const Mapjs = () => {
-        const [map, setMap] = useState(null);
-        const [markers, setMarkers] = useState([]); // Define the markers state variable
-        const [searchInput, setSearchInput] = useState('');
-
-        useEffect(() => {
-            // ComponentDidMount 로직
-            const mapOption = {
-                center: new window.kakao.maps.LatLng(37.542268, 126.967049),
-                level: 3,
-            };
-
-            const initializedMap = new window.kakao.maps.Map(document.getElementById('map'), mapOption);
-            setMap(initializedMap);
-
-            // ... (위 코드에서 변경 없음)
-
-        }, []);
-
-        // 위치 이름 기반 검색 함수
-        function handleSearch() {
-            markers.forEach((marker) => marker.setMap(null)); // 기존 마커 제거
-            setMarkers([]); // 마커 배열 초기화
-
-            for (let i = 0; i < 5000; i++) {
-                const stringVal = window.mapJson.data[i].buildaddress;
-                const substring = searchInput;
-
-                if (stringVal.includes(substring)) {
-                    const markerPosition = new window.kakao.maps.LatLng(window.mapJson.data[i].wgs84lat, window.mapJson.data[i].wgs84lon);
-                    // 마커를 생성합니다
-                    const marker = new window.kakao.maps.Marker({
-                        map: map,
-                        position: markerPosition,
-                        title: window.mapJson.data[i].buildplace,
-                    });
-                    setMarkers((prevMarkers) => [...prevMarkers, marker]);
-                }
-            }
-        }
-
-        const mapStyles = {
-            width: '100%',
-            height: '500px',
-        };
-
-        const searchInputStyles = {
-            width: '200px',
-            height: '30px',
-            marginRight: '10px',
-        };
-
-        const searchButtonStyles = {
-            width: '50px',
-            height: '30px',
-        };
-
-        return (
-            <div>
-                <div id="map" style={mapStyles}></div>
-                <input
-                    type="text"
-                    value={searchInput}
-                    onChange={(e) => setSearchInput(e.target.value)}
-                    style={searchInputStyles}
-                />
-                <button onClick={handleSearch} style={searchButtonStyles}>Search</button>
-            </div>
-        );
-    };
+const mapContainer = document.getElementById('map'); // 지도를 표시할 div 
+const mapOption = {
+    center: new kakao.maps.LatLng(37.542268, 126.967049), // 지도의 중심좌표
+    level: 3 // 지도의 확대 레벨
 };
+
+// 지도를 생성합니다    
+const map = new kakao.maps.Map(mapContainer, mapOption);
+
+// JSON 파싱
+const mydata = mapJson.data;
+let onchecked = false;
+
+const addressArray = [];
+const contentArray = [];
+
+function success({ coords, timestamp }) {
+    const latitude = coords.latitude;   // 위도
+    const longitude = coords.longitude; // 경도
+    alert(`위도: ${latitude}, 경도: ${longitude}, 위치 반환 시간: ${timestamp}`);
+    for (let i = 0; i < 5000; i++) {
+        const tmplat = latitude - mydata[i].wgs84lat;
+        const tmplong = longitude - mydata[i].wgs84lon;
+        if (tmplong < 0) {
+            tmplong *= -1;
+        }
+        if (tmplat < 0) {
+            tmplat *= -1;
+        }
+        const tmpser = tmplat + tmplong;
+
+        if (onchecked) {
+            setMarkers(null);
+        }
+        if (tmpser < 20) {
+            const markerPosition = new kakao.maps.LatLng(mydata[i].wgs84lat, mydata[i].wgs84lon);
+            onchecked = true;
+            const marker = new kakao.maps.Marker({
+                map: map,
+                position: markerPosition,
+                title: mydata[i].buildplace,
+            });
+        }
+    }
+}
+
+function getUserLocation() {
+    if (!navigator.geolocation) {
+        throw "위치 정보가 지원되지 않습니다.";
+    }
+    navigator.geolocation.getCurrentPosition(success);
+}
+
+function search() {
+    for (let i = 0; i < 5000; i++) {
+        const stringVal = mydata[i].buildaddress;
+        const substring = document.getElementById("tbox").value;
+        if (stringVal.indexOf(substring) !== -1) {
+            const markerPosition = new kakao.maps.LatLng(mydata[i].wgs84lat, mydata[i].wgs84lon);
+            const marker = new kakao.maps.Marker({
+                map: map,
+                position: markerPosition,
+                title: mydata[i].buildplace,
+            });
+        }
+    }
+}
+
+function render() {
+    const searchButton = document.querySelector("#searchButton");
+    searchButton.addEventListener('click', search);
+    getUserLocation();
+}
+
+window.onload = render;
