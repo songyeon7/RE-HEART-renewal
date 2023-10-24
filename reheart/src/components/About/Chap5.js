26c0ddc57b22661df480cc6f142c94befa17a2ce
dnwntjsg <97648066+songyeon7@users.noreply.github.com>
2023-10-25 00:00:34 +0900

    방법페이지 추가

---
+++ reheart/src/components/About/Chap5.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from "styled-components";
+import aed from '../../assets/ch5.png';
+import {Contents, MainContainer, MainWrapper, Point, MiniTitle} from "../../atoms/Atomic";
+
+const Chap5 = ({scRef}) => {
+  return (
+    <MainContainer ref={scRef}>
+      <MainWrapper gap={'100px'}>
+        <TempContainer>
+          <div>
+            <MiniTitle >
+            <Point size={'48'} weight={'600'}>1. 전원 켜기</Point> 
+            </MiniTitle>
+            <Blank num={'4vh'}/>
+            <Contents>
+              <div ><br/>
+              먼저 심장충격기(자동제세동기)를 <br/>
+              심폐소생술에 방해가 되지않는 위치에 놓은 뒤에 <br/>
+              전원 버튼을 누른다.<br/><br/>
+              </div>
+            </Contents>
+          </div>
+          <Aed src={aed}/>
+        </TempContainer>
+      </MainWrapper>
+    </MainContainer>
+  );
+};
+
+const Aed = styled.img`
+  width: 300px;
+  height: 300px;
+  flex-shrink: 0;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  @media(max-width: 1080px) {
+    height: 250px;
+    width: 250px;
+  }
+`;
+const Blank = styled.div`
+  width: 0;
+  height: ${(props) => props.num};
+`;
+const TempContainer = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media(max-width: 800px) {
+    flex-direction: column;
+    gap: 20px;
+  }
+`;
+
+export default Chap5;
--- reheart/src/pages/About.js
+++ reheart/src/pages/About.js
@@ -3,6 +3,10 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Prepare from "../components/About/Prepare";
 import Chap1 from '../components/About/Chap1';
+import Chap2 from '../components/About/Chap2';
+import Chap3 from '../components/About/Chap3';
+import Chap4 from '../components/About/Chap4';
+import Chap5 from '../components/About/Chap5';
 
 
 const Main = () => {
@@ -18,6 +22,10 @@ const Main = () => {
       <Header isNotHome={false}/>
       <Prepare scRef={ref}/>
       <Chap1 scRef={ref}/>
+      <Chap2 scRef={ref}/>
+      <Chap3 scRef={ref}/>
+      <Chap4 scRef={ref}/>
+      <Chap5 scRef={ref}/>
       <Footer/>
     </>
   );
