545db885827c8e19912c8dc95058b5f95112c10d
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2023-10-30 15:13:20 +0900

    Update Kakao.js
    쩝..

--- reheart/src/components/Map/Kakao.js
+++ reheart/src/components/Map/Kakao.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const { kakao } = window;
 
 function Kakao() {
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const [map, setMap] = useState(null);
 
     useEffect(() => {
         const container = document.getElementById('map');
@@ -10,36 +12,58 @@ function Kakao() {
             center: new kakao.maps.LatLng(37.542268, 126.967049),
             level: 3
         };
-        const map = new kakao.maps.Map(container, options);
-
-        // 데이터 로드
-        fetch('data.json')
-            .then(response => response.json())
-            .then(data => {
-                // 여기서 데이터를 이용하여 지도에 Marker를 추가하는 작업을 수행
-                data.forEach(item => {
-                    // item에서 위도와 경도 정보 추출
-                    const latLng = new kakao.maps.LatLng(item.latitude, item.longitude);
-
-                    // Marker 추가
-                    const marker = new kakao.maps.Marker({
-                        position: latLng,
-                        map: map
-                    });
-                });
-            })
-            .catch(error => {
-                console.error('데이터를 가져오는 중 에러 발생:', error);
-            });
-
+        const newMap = new kakao.maps.Map(container, options);
+        setMap(newMap);
     }, []);
 
+    const handleSearch = () => {
+        if (searchKeyword.trim() !== '' && map !== null) {
+            const geocoder = new kakao.maps.services.Geocoder();
+
+            geocoder.addressSearch(searchKeyword, function (result, status) {
+                if (status === kakao.maps.services.Status.OK) {
+                    const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                    const marker = new kakao.maps.Marker({
+                        position: coords,
+                        map: map
+                    });
+
+                    map.setCenter(coords);
+                } else {
+                    alert('검색 결과가 없습니다.');
+                }
+            });
+        }
+    };
+
     return (
-        <div id="map" style={{
-            width: '100%',
-            height: '800px'
-        }}></div>
-    ); 
+        <div>
+            <div>
+                <input
+                    type="text"
+                    value={searchKeyword}
+                    onChange={(e) => setSearchKeyword(e.target.value)}
+                    placeholder="주소를 입력하세요"
+                />
+                <button onClick={handleSearch}>검색</button>
+            </div>
+            <div id="map" style={{
+                width: '100%',
+                height: '800px'
+            }}></div>
+        </div>
+    );
 }
+
+// const input = {
+//     position: 'absolute',
+//     top: '20px', 
+//     left: '50%', 
+//     transform: 'translateX(-50%)',
+//     padding: '5px',
+//     fontSize: '16px',
+//     width: '200px',
+// };
 
 export default Kakao;
