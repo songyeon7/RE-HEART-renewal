2e5304742d8af0163e8cd772dddc985a38c72538
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2023-10-30 14:55:30 +0900

    data add, 마커 추가?

--- reheart/src/components/Map/Kakao.js
+++ reheart/src/components/Map/Kakao.js
@@ -11,6 +11,27 @@ function Kakao() {
             level: 3
         };
         const map = new kakao.maps.Map(container, options);
+
+        // 데이터 로드
+        fetch('data.json')
+            .then(response => response.json())
+            .then(data => {
+                // 여기서 데이터를 이용하여 지도에 Marker를 추가하는 작업을 수행
+                data.forEach(item => {
+                    // item에서 위도와 경도 정보 추출
+                    const latLng = new kakao.maps.LatLng(item.latitude, item.longitude);
+
+                    // Marker 추가
+                    const marker = new kakao.maps.Marker({
+                        position: latLng,
+                        map: map
+                    });
+                });
+            })
+            .catch(error => {
+                console.error('데이터를 가져오는 중 에러 발생:', error);
+            });
+
     }, []);
 
     return (
