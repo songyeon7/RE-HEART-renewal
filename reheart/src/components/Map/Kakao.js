51274569a81bc338606e624c1256823851e55b9a
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2023-10-31 23:11:02 +0900

    Update Kakao.js
    될줄알았는데 안됌.ㅜㅠ

--- reheart/src/components/Map/Kakao.js
+++ reheart/src/components/Map/Kakao.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import searchIcon from "../../assets/search.svg";
 import localData from "../../data/data.json";
+import styled from "styled-components";
 
 function Kakao() {
   const [searchKeyword, setSearchKeyword] = useState("");
@@ -85,53 +86,56 @@ function Kakao() {
   }, []); // Call once on component mount
 
   return (
-    <div style={{ position: "relative" }}>
-      <div style={{
-          position: "absolute",
-          top: "150px",
-          left: "50%",
-          transform: "translateX(-50%)",
-          zIndex: "10",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "#fff",
-          border: "1px solid #FF9B9B",
-          borderRadius: "5px",
-        }}>
-        <input
+    <div style={{position: "relative"}}>
+      <Ddiv>
+        <Inputt
           type="text"
           value={searchKeyword}
           onChange={(e) => setSearchKeyword(e.target.value)}
-          placeholder="주소를 입력하세요"
-          style={{
-            width: "1000px",
-            height: "30px",
-            marginRight: "10px",
-            border: "none",
-            padding: "5px",
-            borderRadius: "5px",
-          }}
-        />
-        <button onClick={handleSearch}
-        
-          style={{
-            height: "40px", // 입력 필드 높이에 맞게 조정
-            width: "40px", // 버튼의 가로 크기를 명시적으로 설정
-            color: "transparent",
-            cursor: "pointer",
-            backgroundImage: `url(${searchIcon})`,
-            backgroundSize: "cover", // 이미지가 버튼을 완전히 채우도록
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            border: "none",
-            backgroundColor: "#fff5f5",
-            borderRadius: "0px 5px 5px 0px",
-          }}>Search</button>
-      </div>
+          placeholder="주소를 입력하세요"/>
+        <Buttonn onClick={handleSearch}>Search</Buttonn>
+      </Ddiv>
       <div id="map" style={{ width: "100%", height: "800px" }}></div>
     </div>
   );
 }
+
+
+const Ddiv = styled.div`
+  position: "absolute"
+  top: "150px"
+  left: "50%"
+  transform: "translateX(-50%)"
+  zIndex: "10"
+  display: "flex"
+  alignItems: "center"
+  justifyContent: "center"
+  backgroundColor: "#fff"
+  border: "1px solid #FF9B9B"
+  borderRadius: "5px"
+`;
+
+const Buttonn = styled.div`
+  height: "40px"
+  width: "40px"
+  color: "transparent"
+  cursor: "pointer"
+  backgroundImage: url(${searchIcon})
+  backgroundSize: "cover"
+  backgroundRepeat: "no-repeat"
+  backgroundPosition: "center"
+  border: "none"
+  backgroundColor: "#fff5f5"
+  borderRadius: "0px 5px 5px 0px"
+`;
+
+const Inputt = styled.div`
+  width: "1000px"
+  height: "30px"
+  marginRight: "10px"
+  border: "none"
+  padding: "5px"
+  borderRadius: "5px"
+`;
 
 export default Kakao;
