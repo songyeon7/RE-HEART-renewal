cef5234ecee729a7a7718200faafda193e84084d
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2023-10-31 22:53:24 +0900

    Update Kakao.js
    aed

--- reheart/src/components/Map/Kakao.js
+++ reheart/src/components/Map/Kakao.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import searchIcon from "../../assets/search.svg";
-import localData from "../../data/data.json"; // 로컬 데이터를 임포트합니다.
+import localData from "../../data/data.json";
 
 function Kakao() {
   const [searchKeyword, setSearchKeyword] = useState("");
   const [map, setMap] = useState(null);
   const [markers, setMarkers] = useState([]);
-  const [searchResult, setSearchResult] = useState([]); // 검색 결과 상태를 추가합니다.
+  const [onChecked, setOnChecked] = useState(false);
+  const [myData, setMyData] = useState([]);
 
   useEffect(() => {
     const container = document.getElementById("map");
@@ -17,38 +18,69 @@ function Kakao() {
 
     const newMap = new window.kakao.maps.Map(container, options);
     setMap(newMap);
-    setSearchResult(localData);
+    setMyData(localData.data);
   }, []);
 
-  const handleSearch = () => {
-    if (searchKeyword.trim() !== "" && map !== null) {
-      const geocoder = new window.kakao.maps.services.Geocoder();
-
-      geocoder.addressSearch(searchKeyword, function (result, status) {
-        if (status === window.kakao.maps.services.Status.OK) {
-          // Clear previous markers
-          markers.forEach((marker) => marker.setMap(null));
-
-          const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
-          const marker = new window.kakao.maps.Marker({
-            position: coords,
-            map: map,
-          });
-          setMarkers([marker]);
-
-          map.setCenter(coords);
-        } else {
-          // 사용자 경험을 개선하기 위한 커스텀 모달이나 메시지를 고려해 볼 수 있습니다.
-          console.error("검색 결과가 없습니다.");
-        }
-      });
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success);
+    } else {
+      throw new Error("위치 정보가 지원되지 않습니다.");
     }
   };
 
+  const success = ({ coords, timestamp }) => {
+    const latitude = coords.latitude;
+    const longitude = coords.longitude;
+
+    for (let i = 0; i < 5000; i++) {
+      const tmplat = latitude - myData[i].wgs84lat;
+      const tmplong = longitude - myData[i].wgs84lon;
+
+      // Absolute value for latitude and longitude differences
+      const tmpSer = Math.abs(tmplat) + Math.abs(tmplong);
+
+      if (onChecked === true) {
+        setMarkers([]);
+        setOnChecked(false);
+      }
+      if (tmpSer < 20) {
+        const markerPosition = new window.kakao.maps.LatLng(myData[i].wgs84lat, myData[i].wgs84lon);
+        setOnChecked(true);
+        const marker = new window.kakao.maps.Marker({
+          map: map,
+          position: markerPosition,
+          title: myData[i].buildplace,
+        });
+        setMarkers((prevMarkers) => [...prevMarkers, marker]);
+      }
+    }
+  };
+
+  const handleSearch = () => {
+    if (searchKeyword.trim() !== "" && map !== null) {
+      for (let i = 0; i < 5000; i++) {
+        const stringVal = myData[i].buildaddress;
+        if (stringVal.includes(searchKeyword)) {
+          const markerPosition = new window.kakao.maps.LatLng(myData[i].wgs84lat, myData[i].wgs84lon);
+          const marker = new window.kakao.maps.Marker({
+            map: map,
+            position: markerPosition,
+            title: myData[i].buildplace,
+          });
+          setMarkers((prevMarkers) => [...prevMarkers, marker]);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUserLocation();
+  }, []); // Call once on component mount
+
   return (
     <div style={{ position: "relative" }}>
-      <div
-        style={{
+      <div style={{
           position: "absolute",
           top: "150px",
           left: "50%",
@@ -60,8 +92,7 @@ function Kakao() {
           backgroundColor: "#fff",
           border: "1px solid #FF9B9B",
           borderRadius: "5px",
-        }}
-      >
+        }}>
         <input
           type="text"
           value={searchKeyword}
@@ -76,8 +107,8 @@ function Kakao() {
             borderRadius: "5px",
           }}
         />
-        <button
-          onClick={handleSearch}
+        <button onClick={handleSearch}
+        
           style={{
             height: "40px", // 입력 필드 높이에 맞게 조정
             width: "40px", // 버튼의 가로 크기를 명시적으로 설정
@@ -90,10 +121,7 @@ function Kakao() {
             border: "none",
             backgroundColor: "#fff5f5",
             borderRadius: "0px 5px 5px 0px",
-          }}
-        >
-          Search
-        </button>
+          }}>Search</button>
       </div>
       <div id="map" style={{ width: "100%", height: "800px" }}></div>
     </div>
